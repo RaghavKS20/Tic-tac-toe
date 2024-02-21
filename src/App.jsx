f8c28b54bc6012baf19206a2ee12409eda38c2c1
RaghavKS20 <raghavsahu2009@gmail.com>
2024-02-21 18:09:42 +0530

    completed react tic-tac-toe

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Icon from './components/Icon/Icon'
+import Card from './components/Card/Card'
+import Grid from './components/Grid/Grid'
+
+function App() {
+  return (
+    <div>
+      {/* <Card player=""/> */}
+      <Grid numberOfCards={9}/>
+    </div>
+  )
+}
+
+export default App
